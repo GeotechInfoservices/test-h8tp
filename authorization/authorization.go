6db913a196a014b440bc9cd4bd89fa25c426943b
kasperbe <kasper@firemango.dk>
2019-06-14 13:33:42 +0200

    Fix authorizer

--- authorization/authorization.go
+++ authorization/authorization.go
@@ -39,10 +39,10 @@ type Config struct {
 	UserID func(req request.Request) string
 }
 
-// RequireScope for the supplied context
+// GetOwner for the supplied context
 // Makes sure the user has the necessary scopes to perform the action requested
-func RequireScope(ctx events.APIGatewayProxyRequestContext, scope string) (string, error) {
-	owner, ok := ctx.Authorizer["owner_id"]
+func GetOwner(req request.Request) (string, error) {
+	owner, ok := req.RequestContext.Authorizer["owner_id"]
 	if !ok {
 		return "", MissingOwnerError("invalid token provided")
 	}
@@ -63,21 +63,18 @@ func CurrentUser(ctx events.APIGatewayProxyRequestContext) string {
 // Checks the request context for an owner id and performs checks based on the given config
 func Authorize(h func(request.Request) (events.APIGatewayProxyResponse, error), c Config) func(request.Request) (events.APIGatewayProxyResponse, error) {
 	return func(req request.Request) (events.APIGatewayProxyResponse, error) {
-		userID := c.UserID(req)
 
-		if userID != CurrentUser(req.RequestContext) {
-			_, err := RequireScope(req.RequestContext, c.Scope)
-			if err != nil {
-				logrus.Errorf("%+v", err)
-				return response.Unauthorized("not allowed")
-			}
-		}
-
-		_, err := RequireScope(req.RequestContext, "")
+		owner, err := GetOwner(req)
 		if err != nil {
 			logrus.Errorf("%+v", err)
 			return response.Unauthorized("not allowed")
 		}
+
+		req.RequestContext.Authorizer["owner_id"] = owner
+		// req.RequestContext.Authorizer["role"] = "user"
+
+		// TODO: Handle roles
+		// TODO: Handle correct scope in token, or deny access
 
 		return h(req)
 	}
