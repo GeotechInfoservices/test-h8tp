695bf78d94146e0af32771c5a3c46efac1f80b46
kasperbe <kasper@firemango.dk>
2019-08-21 13:28:00 +0200

    Add response for validation errors

--- response/response.go
+++ response/response.go
@@ -3,9 +3,8 @@ package response
 import (
 	"encoding/json"
 
-	"github.com/asaskevich/govalidator"
 	"github.com/aws/aws-lambda-go/events"
-	"github.com/sirupsen/logrus"
+	"github.com/xsided/h8tp/validation"
 )
 
 // Response use APIGatewayProxyResponse
@@ -112,76 +111,16 @@ func OK(body interface{}) (Response, error) {
 	}, nil
 }
 
-// Error for validation errors.
-type Error struct {
-	Path  string `json:"path"`
-	Error string `json:"error"`
-}
-
-// ValidationError contains path for each error to be associated with
-type ValidationError struct {
-	Message string  `json:"error"`
-	Errors  []Error `json:"validation"`
-}
-
-func (e ValidationError) Error() string {
-	return e.Message
-}
-
-// HandleValidationError recursively walks a tree of errors and flattens them into json path style
-func HandleValidationError(path string, errors error) []Error {
-	var errs []Error
-
-	switch err := errors.(type) {
-	case govalidator.Errors:
-		for _, e := range err.Errors() {
-			errs = append(errs, HandleValidationError(path, e)...)
-		}
-	case govalidator.Error:
-		path += "."
-		path += err.Name
-
-		return []Error{
-			{
-				Path:  path,
-				Error: err.Error(),
-			},
-		}
-	default:
-		return errs
-	}
-
-	return errs
-}
-
-// BadInput implies an error in the input, according to the entity validation rules.
-func BadInput(errors error) (Response, error) {
-	out := ValidationError{
-		Message: "invalid input",
-	}
-	switch err := errors.(type) {
-	case govalidator.Errors:
-		out.Errors = HandleValidationError("$", err)
-	default:
-		logrus.Errorf("Error while handling Bad Input. %+v", err)
-		e, _ := json.Marshal(map[string]string{
-			"message": "internal server error",
-		})
-
-		return Response{
-			Headers: map[string]string{
-				"Access-Control-Allow-Credentials": "true",
-				"Access-Control-Allow-Origin":      "*",
-			},
-			StatusCode: 500,
-			Body:       string(e),
-		}, nil
-
-	}
-
-	raw, err := json.Marshal(out)
+// ValidationError for handling validation errors
+func ValidationError(resp validation.Error) (Response, error) {
+	b, err := json.Marshal(resp)
 	if err != nil {
-		// TODO: Do some checking
+		msg := map[string]string{"message": "error while retrieving items"}
+		b, _ := json.Marshal(msg)
+		return Response{
+			StatusCode: 500,
+			Body:       string(b),
+		}, nil
 	}
 
 	return Response{
@@ -190,7 +129,6 @@ func BadInput(errors error) (Response, error) {
 			"Access-Control-Allow-Origin":      "*",
 		},
 		StatusCode: 400,
-		Body:       string(raw),
+		Body:       string(b),
 	}, nil
-
 }
